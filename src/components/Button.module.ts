3854e7ecc4abb50c3d9978d3543ec3f0722b7d48
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-07-24 09:01:50 -0300

    add style files
    create firts scripts of style

--- src/components/Button.module.ts
+++ src/components/Button.module.ts
@@ -18,6 +18,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border-radius: 4px;
   border: 0;
   margin: 8px;
-  background-color: ${(props) => props.theme.primary};
-  color: ${(props) => props.theme.primary};
+  background-color: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme.white};
 `;
